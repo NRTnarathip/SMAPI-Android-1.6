82a6455ec6329fab61a27af1ebae9f06cffa976a
NRTnarathip <61742272+NRTnarathip@users.noreply.github.com>
2024-12-05 03:16:59 +0700

    Fix: GetColorData & GetData and optimize

--- src/SMAPI/Mobile/Facade/Texture2DRewriter.cs
+++ src/SMAPI/Mobile/Facade/Texture2DRewriter.cs
@@ -1,43 +1,100 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Android.Hardware.Lights;
 using HarmonyLib;
+using Java.Lang;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Mono.Cecil;
 using Mono.Cecil.Cil;
+using StardewModdingAPI.Framework;
 using StardewModdingAPI.Framework.ModLoading.Rewriters;
+using StbImageWriteSharp;
 using static Android.Icu.Text.ListFormatter;
 
 namespace StardewModdingAPI.Mobile.Facade;
 [HarmonyPatch]
 public static class Texture2DRewriter
 {
-    public static void GetData(this Texture2D texture, Color[] data)
+    static void CropPixels(
+        Color[] srcPixels, Color[] dstPixels,
+        int srcWidth, int srcHeight,
+        int dstWidth, int dstHeight)
     {
-        //Console.WriteLine("Hook GetData()");
-        //Console.WriteLine("texture: " + texture.Name);
-        //Console.WriteLine("texture size width: " + texture.Width);
-        //Console.WriteLine("texture size height: " + texture.Height);
-        //Console.WriteLine("data length: " + data.Length);
+        //Console.WriteLine($"Crop Pixel: src size: {srcWidth} x {srcHeight}");
+        //Console.WriteLine($"Crop Pixel: dst size: {dstWidth} x {dstHeight}");
 
-        //remap data size
-        Color[] dataWithActualSize = new Color[texture.ActualWidth * texture.ActualHeight];
+        //use simple loop
+        if (dstWidth <= 128 && dstHeight <= 128)
+        {
+            for (int y = 0; y < dstHeight; y++)
+            {
+                for (int x = 0; x < dstWidth; x++)
+                {
+                    int sourceIndex = y * srcWidth + x;
+                    int targetIndex = y * dstWidth + x;
+                    dstPixels[targetIndex] = srcPixels[sourceIndex];
+                }
+            }
 
-        texture.GetData(dataWithActualSize);
+            return;
+        }
 
-        //TODO: Work in progress
-        //not sure
+        //check if size height more than width
+        //we should ForEach in y
+        //!! if it's same size we don't crop,
+        //we just clone it
 
-        //clone data
-        Array.Copy(dataWithActualSize, data, data.Length);
+        if (dstHeight > dstWidth)
+        {
+            Parallel.For(0, dstHeight, y =>
+            {
+                int sourceIndex = y * srcWidth;
+                int targetIndex = y * dstWidth;
+                Array.Copy(srcPixels, sourceIndex, dstPixels, targetIndex, dstWidth);
+            });
+        }
+        else if (dstHeight < dstWidth)
+        {
+            Parallel.For(0, dstWidth, x =>
+            {
+                int sourceIndex = x * srcHeight;
+                int targetIndex = x * dstHeight;
+                Array.Copy(srcPixels, sourceIndex, dstPixels, targetIndex, dstHeight);
+            });
+        }
+        else //it same size
+        {
+            Array.Copy(srcPixels, dstPixels, dstPixels.Length);
+        }
+    }
+    public static void Fix_GetDataForPC(this Texture2D texture, Color[] data)
+    {
+        if (texture.ActualWidth == texture.Width
+            && texture.ActualHeight == texture.Height)
+        {
+            //correct size
+            texture.GetData(data);
+            return;
+        }
+
+        //correct to data pixels
+        Color[] srcPixels = new Color[texture.ActualWidth * texture.ActualHeight];
+        texture.GetData(srcPixels);
+
+        //crop image
+        CropPixels(srcPixels, data,
+            texture.ActualWidth, texture.ActualHeight,
+            texture.Width, texture.Height);
     }
 
-    public static MethodInfo GetData_Color_MethodInfo_New => AccessTools.Method(typeof(Texture2DRewriter), nameof(GetData));
+    public static MethodInfo GetData_Color_MethodInfo_New => AccessTools.Method(typeof(Texture2DRewriter), nameof(Fix_GetDataForPC));
     public const string GetData_FullNameStartWith = "System.Void Microsoft.Xna.Framework.Graphics.Texture2D::GetData";
     public static bool GetData_Callback(
         MapMethodWithCallback mapMethod,
@@ -53,18 +110,12 @@ public static class Texture2DRewriter
         return false;
     }
 
-    static List<Texture2D> ListAllTextureFrom_FixCopyFromTexture = new();
     [HarmonyPrefix]
     [HarmonyPatch(typeof(Texture2D), nameof(Texture2D.CopyFromTexture))]
     static bool FixCopyFromTexture(Texture2D __instance, Texture2D other)
     {
         var srcTexture = other;
         var dstTexture = __instance;
-        //Console.WriteLine("try fix copy from texture");
-        //Console.WriteLine($"other width: {srcTexture.Width}, height: {srcTexture.Height}");
-        //Console.WriteLine($"other actual width: {srcTexture.ActualWidth},actual height: {srcTexture.ActualHeight}");
-        //Console.WriteLine($"try copy to: width {dstTexture.Width}, height: {dstTexture.Height}");
-        //Console.WriteLine($"try copy to: actual width {dstTexture.ActualWidth},actual height: {dstTexture.ActualHeight}");
 
         //get data, android use size power two
         Color[] dataInActualSize = new Color[other.ActualWidth * other.ActualHeight];
@@ -86,9 +137,37 @@ public static class Texture2DRewriter
         TexelHeight_Prop.SetValue(dstTexture, texelHeight);
 
         dstTexture.SetData(dataInActualSize);
+        //done Fix CopyFromTexture
 
-        ListAllTextureFrom_FixCopyFromTexture.Add(dstTexture);
-        Console.WriteLine("done fix clone texture: " + dstTexture.Name);
+
+
+        //tester only
+        string saveOutputDir = Path.Combine(EarlyConstants.ExternalFilesDir, "Export Png");
+        //save into file
+        string originalFilePath = Path.Combine(saveOutputDir, $"{srcTexture.Name}_src.png");
+        //safe file path
+        string dir = Path.GetDirectoryName(originalFilePath);
+        if (Directory.Exists(dir) == false)
+            Directory.CreateDirectory(dir);
+
+        //original
+        using var imgOriginalStream = File.Create(originalFilePath);
+        srcTexture.SaveAsPng(imgOriginalStream, srcTexture.ActualWidth, srcTexture.ActualHeight);
+
+        //clone
+        using var imgCloneStream = File.Create(
+            Path.Combine(saveOutputDir, $"{dstTexture.Name}_clone.png"));
+        dstTexture.SaveAsPng(imgCloneStream, dstTexture.ActualWidth, dstTexture.ActualHeight);
+
+        int width = dstTexture.Width;
+        int height = dstTexture.Height;
+        var textureCut = new Texture2D(StardewValley.Game1.graphics.GraphicsDevice, width, height);
+        textureCut.SetName($"{dstTexture.Name}_pixel_cut");
+        using var imgPixelCutStream = File.Create(
+            Path.Combine(saveOutputDir, $"{textureCut.Name}_pixelCut.png"));
+        var pixelsCut = new Color[width * height];
+        dstTexture.Fix_GetDataForPC(pixelsCut);
+        textureCut.SavePngWithPixel(pixelsCut, imgPixelCutStream, width, height);
 
         return false;
     }
@@ -98,4 +177,42 @@ public static class Texture2DRewriter
     static PropertyInfo ActualHeight_Prop = AccessTools.Property(typeof(Texture2D), "ActualHeight");
     static FieldInfo width_Field = AccessTools.Field(typeof(Texture2D), "width");
     static FieldInfo height_Field = AccessTools.Field(typeof(Texture2D), "height");
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(Texture2D), "GetColorData")]
+    static bool Fix_GetColorData(ref Color[] __result, Texture2D __instance)
+    {
+        var texture = __instance;
+        switch (texture.Format)
+        {
+            case SurfaceFormat.Color:
+                Color[] pixels = new Color[texture.ActualWidth * texture.ActualHeight];
+                texture.GetData(pixels);
+                //return value
+                __result = pixels;
+                //skip original method
+                return false;
+        }
+
+        return true;
+    }
+    static unsafe void FixSavePng(this Texture2D texture, Stream stream, int width, int height)
+    {
+        Color[] pixels = null;
+        Fix_GetColorData(ref pixels, texture);
+        fixed (Color* data = &pixels[0])
+        {
+            ImageWriter imageWriter = new ImageWriter();
+            imageWriter.WritePng(data, width, height, ColorComponents.RedGreenBlueAlpha, stream);
+        }
+    }
+    static unsafe void SavePngWithPixel(this Texture2D texture, Color[] pixels, Stream stream, int width, int height)
+    {
+        fixed (Color* data = &pixels[0])
+        {
+            ImageWriter imageWriter = new ImageWriter();
+            imageWriter.WritePng(data, width, height, ColorComponents.RedGreenBlueAlpha, stream);
+        }
+    }
+
+
 }
