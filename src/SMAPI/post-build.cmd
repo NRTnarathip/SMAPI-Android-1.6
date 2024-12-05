8201811f688ba9205b284af98320ac92b384dc87
NRTnarathip <61742272+NRTnarathip@users.noreply.github.com>
2024-12-06 01:07:26 +0700

    Fix: post build

--- src/SMAPI/post-build.cmd
+++ src/SMAPI/post-build.cmd
@@ -1,8 +1,13 @@
 
-adb logcat -c
 set AppName=abc.smapi.gameloader
 
 adb shell am force-stop %AppName%
+
+if errorlevel 1 (
+    echo ADB is not connected.
+    exit /b
+)
+
 
 adb push "bin/ARM64/Android Release/StardewModdingAPI.dll" "/storage/emulated/0/Android/data/%AppName%/files/Stardew Assemblies/StardewModdingAPI.dll"
 
